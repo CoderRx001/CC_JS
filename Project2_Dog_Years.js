7cfc5a5ba0c98b46c1cc3630feccb147698f21b5
CoderRx001 <carrillo964@gmail.com>
2018-07-17 01:56:05 -0500

    CC work JS

---
+++ Project2_Dog_Years.js
@@ -0,0 +1,19 @@
+// my current age
+const myAge = 60;
+
+//first 2 yrs eaqual 10.5
+let earlyYears = 2;
+earlyYears = earlyYears * 10.5;
+
+// the later years
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears, laterYears);
+
+// combining the equations for the sum
+myAgeInDogYears = earlyYears + laterYears;
+
+myName = "Anthony".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
